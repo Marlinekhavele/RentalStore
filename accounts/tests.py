3fe9400ee6437ac0a998454da4dc59c3759c8feb
MarlineKhavele <marlinekhavele6@gmail.com>
2021-01-03 01:36:54 +0300

    fixed accounts tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,7 +8,7 @@ class CustomerAccountTests(APITestCase):
     def test_create_account(self):
 
         url = reverse('customer-list')
-        data = {'username': 'beastkhavele','email':'beast@gmail.com'}
+        data = {'username': 'beast','email':'beast@gmail.com'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Customer.objects.count(), 1)
